bb3c2016214eb265552a27ab2f6a4c857c511871
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2025-04-25 21:08:20 -0400

    Add files via upload

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,11 +2,19 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 class CustomLoginForm(AuthenticationForm):
-    username = forms.EmailField(
-        label="Correo Electrónico", 
-        widget=forms.EmailInput(attrs={'placeholder': 'Correo electrónico'})
-    )
-    password = forms.CharField(
-        label="Contraseña", 
-        widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña'})
-    )
+     username = forms.EmailField(
+         label="Correo Electrónico",
+
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Correo electrónico',
+            'autocomplete': 'email',
+        })
+     )
+     password = forms.CharField(
+         label="Contraseña",
+
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Contraseña',
+            'autocomplete': 'current-password',
+        })
+     )
