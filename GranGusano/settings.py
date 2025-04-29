df246a3d75c1dc16a2de11fda19e934d0a7bf67b
weberest <felipeestradawevar@gmail.com>
2025-04-29 17:32:20 -0400

    se agrego funcionalidad de recuperar contraseña con correo

--- GranGusano/settings.py
+++ GranGusano/settings.py
@@ -152,8 +152,13 @@ ACCOUNT_SIGNUP_REDIRECT_URL = '/accounts/login/'
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 LOGIN_URL = '/accounts/login/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+# considerar ocupar varibles de entorno mas adelante
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'clubdelgrangusanoweb@gmail.com'
+EMAIL_HOST_PASSWORD = 'kqjk eoxk ulfk vfmc'
 
