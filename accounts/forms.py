4216906479269687fb7fe204c8c7c79b09bd93c2
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2025-05-05 09:23:05 -0400

    Add files via upload

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm, UserCreationForm
 from django.contrib.auth.models import User
+from allauth.account.forms import SignupForm
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(
+        max_length=30,
+        label="Nombre",
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Tu nombre'
+        })
+    )
+    
+    def save (self, request):
+        user = super().save(request)
+        user.first_name = self.cleaned_data ['first_name']
+        user.save()
+        return user
 
 class CustomLoginForm(AuthenticationForm):
      username = forms.EmailField(
@@ -23,7 +41,7 @@ class CustomLoginForm(AuthenticationForm):
 
 class PerfilForm(forms.ModelForm):
     # Añadimos los campos para la edición del perfil
-    username = forms.CharField(max_length=150,label="Nombre de usuario",required=True,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # username = forms.CharField(max_length=150,label="Nombre de usuario",required=True,widget=forms.TextInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(max_length=30, label="Nombre", required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=30, label="Apellido", required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label="Correo electrónico", widget=forms.EmailInput(attrs={'class': 'form-control'}))
@@ -42,7 +60,8 @@ class PerfilForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username','first_name', 'last_name', 'email']
+        # fields = ['username','first_name', 'last_name', 'email']
+        fields = ['first_name', 'last_name']
 
     # Validación para la contraseña
     def clean(self):
