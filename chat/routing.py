85f13ba3ab4d2cf68744d7106df8df0e0ec7bb0e
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2025-06-08 14:32:56 -0400

    Add files via upload

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,8 @@
+# chat/routing.py
 from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
+    # Sala única: ws://.../ws/chat/
+    re_path(r'^ws/chat/$', consumers.ChatConsumer.as_asgi()),
 ]
