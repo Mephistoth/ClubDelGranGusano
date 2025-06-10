bc3ceefc95fc4a77c23a9c717bff079182e74eb3
weberest <felipeestradawevar@gmail.com>
2025-06-09 23:29:27 -0400

    configuraciones para render

--- accounts/apps.py
+++ accounts/apps.py
@@ -6,3 +6,13 @@ class AccountsConfig(AppConfig):
 
     def ready(self):
         import accounts.signals
+
+        from django.apps import AppConfig
+
+class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
+
+    def ready(self):
+        from .create_site import create_default_site
+        create_default_site()
