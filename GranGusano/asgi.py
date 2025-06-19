bf1eee0099c40a29537d08fbf1f0df9dfb6f4d60
Dabrxx <166577194+Dabrxx@users.noreply.github.com>
2025-06-18 21:32:48 -0400

    Update asgi.py

--- GranGusano/asgi.py
+++ GranGusano/asgi.py
@@ -1,20 +1,13 @@
 import os
-
-# 1) Define SETTINGS MODULE antes de TODO
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GranGusano.settings")
 
-# 2) Importa y ejecuta get_asgi_application, cargando así las apps
 from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
-
-# 3) Ahora las apps están listas: importa Channels y tu routing
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
 
-# 4) Monta el router ASGI para HTTP y WebSocket
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
